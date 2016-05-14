b13eb07151636d39777c8ef74e5d3e27f2af4e1d
sauravtom <stomatrix@gmail.com>
2016-05-15 03:15:57 +0800

    final changes

--- chase/video_maker.py
+++ chase/video_maker.py
@@ -6,7 +6,7 @@ import pyvona
 import re
 
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
-NUMBER_OF_IMAGES = 3
+NUMBER_OF_IMAGES = 5
 UNDERCOLOR = 'rgba(0,0,0,0.3)'
 FILLCOLOR = 'rgba(251,251,255)'
 FONT_LOC = '%s/design_assets/TisaPro.otf'%DIR_PATH
@@ -15,24 +15,13 @@ v = pyvona.create_voice('GDNAJJ2TZFHSNAJAEYHA', 'vOgSfcz88uZxElIU2K5PLAgWfIJiajo
 
 
 def summarize(text):
-	text = text.split('.')[:3]
+	text = text.split('.')
 	text = ".".join(text)
 	#remove text in round brackets and square brackets
 	text = re.sub(r'\([^)]*\)', '', text)
 	text = re.sub(r'\[[^)]*\]', '', text)
+	text = text[:1050]
 	return text
-
-def wikivideo(query='New York'):
-	ny = wikipedia.page(query)
-	content = ny.content
-	summary = summarize(content)
-	
-	download_images(query,NUMBER_OF_IMAGES)
-
-	generate_voice(summary)
-
-	#bake the oven
-
 
 def bake():
 	for counter in range(NUMBER_OF_IMAGES+1):
@@ -94,10 +83,7 @@ def bake2():
 	os.system("ffmpeg -i %s/oven/final.gif -vcodec libx264 -vf 'scale=trunc(iw/2)*2:trunc(ih/2)*2' \
 	    -pix_fmt yuv420p -movflags +faststart %s/oven/final.mp4"%(DIR_PATH,DIR_PATH))
 		
-
-
-
-def donwload_images(query,number='6'):
+def download_images(query,number='6'):
 	os.system('rm %s/oven/*'%(DIR_PATH))
 	os.system('node download_images.js "%s" %s'%(query,number))
 
@@ -107,9 +93,24 @@ def generate_voice(text='hello world'):
 	v.fetch_voice(text, '%s/oven/narration'%(DIR_PATH))
 
 
+def main(query='New York'):
+	ny = wikipedia.page(query)
+	content = ny.content
+	summary = summarize(content)
+	#download_images(query,NUMBER_OF_IMAGES)
+
+	generate_voice(summary)
+
+	#bake the oven
+	bake()
+	print summary
+
+
 
 if __name__ == '__main__':
-	bake()
+	#download_images(query='android',NUMBER_OF_IMAGES)
+	main()
+	#bake()
 	#print summarize("asdasdasdasd(123).asdas[123123]dasdasd.asdasdasd.asdasd.123142134.234234234.234234423")
 	#generate_voice()
 	#wikivideo()

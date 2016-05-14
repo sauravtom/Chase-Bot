48c1b88b811c72f6d565c88d5966669f4b01433a
sauravtom <stomatrix@gmail.com>
2016-05-15 04:14:59 +0800

    more daym changes

--- chase/video_maker.py
+++ chase/video_maker.py
@@ -4,6 +4,7 @@ import wikipedia
 import os
 import pyvona
 import re
+import sys
 
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 NUMBER_OF_IMAGES = 5
@@ -27,24 +28,24 @@ def bake():
 	for counter in range(NUMBER_OF_IMAGES+1):
 		title = 'food chamber'
 		#normalize the dimensions of the png files
-		os.system("convert %s/oven/slide_%s.png \( -clone 0 -blur 0x15 -resize 480x480\! \) \( -clone 0 -resize 480x480 \) -delete 0 \
-		    -gravity center -compose over -composite %s/oven/slide_%s.png"%(DIR_PATH,counter,DIR_PATH,counter))
+		os.system("convert %s/oven/temp/slide_%s.png \( -clone 0 -blur 0x15 -resize 480x480\! \) \( -clone 0 -resize 480x480 \) -delete 0 \
+		    -gravity center -compose over -composite %s/oven/temp/slide_%s.png"%(DIR_PATH,counter,DIR_PATH,counter))
 
 		#generate caption files
 		os.system("convert -size %sx%s -background 'rgba(154,78,225,0.4)' -font %s \
 		    -fill '%s' -gravity West  \
 		    -bordercolor 'rgba(154,78,225,0.4)' -border 25x25 \
-		 caption:'%s' -flatten %s/oven/caption_%s.png"%(375,480/3-70,FONT_LOC,FILLCOLOR,title.upper(),DIR_PATH,counter))
+		 caption:'%s' -flatten %s/oven/temp/caption_%s.png"%(375,480/3-70,FONT_LOC,FILLCOLOR,title.upper(),DIR_PATH,counter))
 
 		#adding captions to slides
-		os.system("composite -gravity South %s/oven/caption_%s.png %s/oven/slide_%s.png %s/oven/slide_%s.png"%(DIR_PATH,counter,DIR_PATH,counter,DIR_PATH,counter))
+		os.system("composite -gravity South %s/oven/temp/caption_%s.png %s/oven/temp/slide_%s.png %s/oven/temp/slide_%s.png"%(DIR_PATH,counter,DIR_PATH,counter,DIR_PATH,counter))
 
-	os.system("ffmpeg -i %s/oven/slide_%%d.png -vcodec mpeg4 %s/oven/video_fast.mp4"%(DIR_PATH,DIR_PATH))
-	os.system('ffmpeg -i %s/oven/video_fast.mp4 -vf "setpts=(150)*PTS" %s/oven/final_output.mp4'%(DIR_PATH,DIR_PATH))
+	os.system("ffmpeg -i %s/oven/temp/slide_%%d.png -vcodec mpeg4 %s/oven/temp/video_fast.mp4"%(DIR_PATH,DIR_PATH))
+	os.system('ffmpeg -i %s/oven/temp/video_fast.mp4 -vf "setpts=(150)*PTS" %s/oven/temp/final_output.mp4'%(DIR_PATH,DIR_PATH))
 
 	#add narration to video
-	os.system("ffmpeg -i %s/oven/final_output.mp4 -i %s/oven/narration.mp3 \
-        %s/oven/0final.mp4"%(DIR_PATH,DIR_PATH,DIR_PATH))
+	os.system("ffmpeg -i %s/oven/temp/final_output.mp4 -i %s/oven/temp/narration.mp3 \
+        %s/oven/temp/0final.mp4"%(DIR_PATH,DIR_PATH,DIR_PATH))
 
 def bake2():
 	for counter in range(NUMBER_OF_IMAGES):
@@ -84,32 +85,43 @@ def bake2():
 	    -pix_fmt yuv420p -movflags +faststart %s/oven/final.mp4"%(DIR_PATH,DIR_PATH))
 		
 def download_images(query,number='6'):
-	os.system('rm %s/oven/*'%(DIR_PATH))
 	os.system('node download_images.js "%s" %s'%(query,number))
 
 def generate_voice(text='hello world'):
 	v.codec = 'mp3'
 	v.voice_name = 'Raveena'
-	v.fetch_voice(text, '%s/oven/narration'%(DIR_PATH))
+	v.fetch_voice(text, '%s/oven/temp/narration'%(DIR_PATH))
 
 
 def main(query='New York'):
-	ny = wikipedia.page(query)
-	content = ny.content
-	summary = summarize(content)
+	os.system('mkdir %s/oven/temp/'%(DIR_PATH))
+	page_name = wikipedia.search(query, results=1)
+
+	try:
+		result = wikipedia.page(page_name[0])
+	except wikipedia.exceptions.DisambiguationError as e:
+		result = wikipedia.page(e.options[0])
+
+	text = result.content
+	summary = summarize(text)
+
 	#download_images(query,NUMBER_OF_IMAGES)
-
 	generate_voice(summary)
-
 	#bake the oven
 	bake()
-	print summary
+
+	folder_name = query.replace(" ",'_')
+	folder_name = folder_name.lower()
+	folder_name = folder_name.strip()
+	os.system("mv %s/oven/temp %s/oven/%s"%(DIR_PATH,DIR_PATH,folder_name))
+
 
 
 
 if __name__ == '__main__':
+	query = sys.argv[1]
 	#download_images(query='android',NUMBER_OF_IMAGES)
-	main()
+	main(query)
 	#bake()
 	#print summarize("asdasdasdasd(123).asdas[123123]dasdasd.asdasdasd.asdasd.123142134.234234234.234234423")
 	#generate_voice()
